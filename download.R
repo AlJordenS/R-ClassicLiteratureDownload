923ac6c5113465f1253ba4e735934ed811c7cddc
AlJordenS <aljordenbs@gmail.com>
2021-07-07 14:54:35 -0500

    First two tasks of Pluralsight Lab

--- download.R
+++ download.R
@@ -4,3 +4,8 @@ library(stringr)
 library(ggplot2)
 
 books <- read_csv('data/books.csv')
+
+twain <- filter(books, author == "Twain, Mark")
+
+twain_refined <- twain %>% select(title, author, downloads, avg_words_per_sentence, sentences) 
+
